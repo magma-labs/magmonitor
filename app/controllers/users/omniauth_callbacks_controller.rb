bbad81086aa037944eccee43691e830518cb1635
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2017-07-10 17:28:25 -0500

    Refactor omniauth callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,18 +3,20 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def github
-      user = Magmonitor::UserRegistration::GithubService.new(request.env['omniauth.auth']).persist
-      sign_in_and_redirect user, event: :authentication
+      perform_registration(Magmonitor::UserRegistration::GithubService)
     end
 
     def google_oauth2
-      user = Magmonitor::UserRegistration::GoogleOauth2Service
-          .new(request.env['omniauth.auth']).persist
-      sign_in_and_redirect user, event: :authentication
+      perform_registration(Magmonitor::UserRegistration::GoogleOauth2Service)
     end
 
     def failure
       redirect_to root_path
+    end
+
+    def perform_registration(klass)
+      user = klass.new(request.env['omniauth.auth']).persist
+      sign_in_and_redirect user, event: :authentication
     end
   end
 end
--- app/services/magmonitor/user_registration/base.rb
+++ app/services/magmonitor/user_registration/base.rb
@@ -13,6 +13,7 @@ module Magmonitor
         User.find_or_create_by(email: user_email) do |user|
           user.name = user_name
           user.image = user_image
+          user.password = Devise.friendly_token[0, 20]
         end
       end
     end
