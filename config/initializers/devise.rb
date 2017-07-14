cf52a2d7c72aafd30d974b9c5d7144abe34cacec
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2017-07-14 10:47:11 -0500

    Add magmonitor@magmalabs.io mail to devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'com'
+  config.mailer_sender = 'magmonitor@magmalabs.io'
   config.mailer = 'MagMonitorMailer'
 
   # Configure the class responsible to send e-mails.
