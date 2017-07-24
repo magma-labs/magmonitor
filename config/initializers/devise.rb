3a9de0cf6fe29d6849e6e7854b7dae1b9f0299b8
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2017-07-23 20:05:17 -0500

    Update sender email temporary (until magmonitor email is created)

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,8 +14,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'magmonitor@magmalabs.io'
-  config.mailer = 'MagMonitorMailer'
+  config.mailer_sender = 'jose.espinoza@magmalabs.io'
+  config.mailer = 'MagmonitorMailer'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
