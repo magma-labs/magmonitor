af2fa5ec68d53895927a47320e737dacddd1f875
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2017-07-31 10:21:44 -0500

    Remove InvitesController, code was move into OrganizationsController

--- app/controllers/invites_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class InvitesController < ApplicationController
-  before_action :authenticate_user!, :check_user_registration
-  before_action :allowed_to_invite?, only: %i[index create]
-
-end
